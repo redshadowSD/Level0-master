1edfa94c435bb903d17b38bb5b5d51561b112e0e
Wintress Mac <DON'T CHANGE THIS>
2016-06-18 09:59:17 -0700

    new
    2 new code

---
+++ src/ShinyObjects.java
@@ -0,0 +1,40 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class ShinyObjects {
+
+	public static void main(String[] args) {
+		// 2. Ask the user how many shiny objects they want
+
+		// 3. Play the sound that many times
+
+		// 1. Call the method below
+		String a = JOptionPane.showInputDialog("How many shiny objects do you want?");
+		int b = Integer.parseInt(a);
+		MisterZee(b);
+	}
+
+	public static void playMisterZee() {
+		try {
+			File soundFile = new File("/Users/League/Google Drive/league-sounds/shiny-objects.wav");
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void MisterZee(int Zee) {
+		for (int i = 0; i < Zee; i++) {
+			playMisterZee();
+		}
+	}
+
+}
